2a06a32dc149cfab76cfd4f2012a85dcc79497c9
Ivan <ikladinov@gmail.com>
2022-12-15 13:27:27 -0500

    commit tree 902ff12eba0b25d347ef637c492cb4d3e395df8c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(name) {
+    for (let i = 0; i < name.length; i++){
+        console.log(`Thank you, ${name[i]} , for the wonderful surprise gift!`);
+    }
+
+    return name;
+}
+
+writeCards(name);
+
